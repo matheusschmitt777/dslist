ea097cfd43305565bc1b422b97a100dd4de79e5b
Matheus Schmitt <matheusschmittmr45@gmail.com>
2023-07-11 22:46:02 -0300

    GET game by id

--- src/main/java/com/matheusdev/dslist/services/GameService.java
+++ src/main/java/com/matheusdev/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.matheusdev.dslist.dto.GameDTO;
 import com.matheusdev.dslist.dto.GameMinDTO;
 import com.matheusdev.dslist.entities.Game;
 import com.matheusdev.dslist.repositories.GameRepository;
@@ -15,6 +17,13 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(GameMinDTO::new).toList();
