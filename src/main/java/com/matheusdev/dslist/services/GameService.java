8389f56d380dd22b5dc64a1695b5ed72148d290d
Matheus Schmitt <matheusschmittmr45@gmail.com>
2023-07-11 23:37:58 -0300

    SQL, projection, get games by list

--- src/main/java/com/matheusdev/dslist/services/GameService.java
+++ src/main/java/com/matheusdev/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.matheusdev.dslist.dto.GameDTO;
 import com.matheusdev.dslist.dto.GameMinDTO;
 import com.matheusdev.dslist.entities.Game;
+import com.matheusdev.dslist.projections.GameMinProjection;
 import com.matheusdev.dslist.repositories.GameRepository;
 
 @Service
@@ -21,6 +22,12 @@ public class GameService {
 	public GameDTO findById(Long id) {
 		Game result = gameRepository.findById(id).get();
 		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(GameMinDTO::new).toList();
 	}
 	
 	@Transactional(readOnly = true)
